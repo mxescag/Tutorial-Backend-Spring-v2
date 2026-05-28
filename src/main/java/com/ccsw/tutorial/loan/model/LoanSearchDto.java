600575dd8a89b00d22e7a99606b188783d1995eb
Max Escriva <max.escriva-aguado@capgemini.com>
2026-05-28 12:20:28 +0200

    fix: add private attributes

--- src/main/java/com/ccsw/tutorial/loan/model/LoanSearchDto.java
+++ src/main/java/com/ccsw/tutorial/loan/model/LoanSearchDto.java
@@ -2,12 +2,17 @@ package com.ccsw.tutorial.loan.model;
 
 import com.ccsw.tutorial.common.pagination.PageableRequest;
 
+import java.time.LocalDate;
+
 /**
  * @author Max Escrivá
  */
 public class LoanSearchDto {
     /* -- ATRIBUTOS -- */
     private PageableRequest pageable;
+    private Long gameId;
+    private Long clientId;
+    private LocalDate date;
 
     /* -- GETTERS Y SETTERS -- */
     public PageableRequest getPageable() {
@@ -17,4 +22,15 @@ public class LoanSearchDto {
     public void setPageable(PageableRequest pageable) {
         this.pageable = pageable;
     }
+
+    /* Añadimos campos de filtro, si no, siempre devolverá resultado sin filtrar. */
+
+    public Long getGameId() { return gameId; }
+    public void setGameId(Long gameId) { this.gameId = gameId; }
+
+    public Long getClientId() { return clientId; }
+    public void setClientId(Long clientId) { this.clientId = clientId; }
+
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
 }
